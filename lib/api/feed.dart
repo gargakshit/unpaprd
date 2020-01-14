99aff9402dec973fb7b6d45ace71cd7c17eead71
Akshit Garg <garg.akshit@gmail.com>
2020-01-14 18:15:32 +0530

    Remove Old Player Add genres in Explore

--- lib/api/feed.dart
+++ lib/api/feed.dart
@@ -3,9 +3,8 @@ import 'package:http/http.dart';
 
 import 'package:unpaprd/models/feed.dart';
 
-Future<AudiobookShortList> fetchFeed({int limit: 10}) async {
-  final response =
-      await get("https://unpaprdapi.gargakshit.now.sh/api/feed?limit=$limit");
+Future<AudiobookShortList> fetchFeed(String url, {int limit: 10}) async {
+  final response = await get("$url?limit=$limit");
 
   if (response.statusCode == 200) {
     return AudiobookShortList.fromJson(jsonDecode(response.body));
--- lib/screens/explore.dart
+++ lib/screens/explore.dart
@@ -13,20 +13,46 @@ class ExplorePage extends StatefulWidget {
   _ExplorePageState createState() => _ExplorePageState();
 }
 
-class _ExplorePageState extends State<ExplorePage> {
+class _ExplorePageState extends State<ExplorePage>
+    with SingleTickerProviderStateMixin {
   Future<AudiobookShortList> feed;
+  Future<AudiobookShortList> lifi;
+  Future<AudiobookShortList> romance;
+  Future<AudiobookShortList> plays;
+  Future<AudiobookShortList> scifi;
+  Future<AudiobookShortList> poetry;
+  Future<AudiobookShortList> satire;
+
+  TabController controller;
+
+  @override
+  void dispose() {
+    super.dispose();
+
+    controller.dispose();
+  }
 
   @override
   void initState() {
     super.initState();
 
-    feed = fetchFeed(limit: 10);
-  }
+    controller = TabController(length: 7, vsync: this);
 
-  Future<void> refresh() async {
-    setState(() {
-      feed = fetchFeed(limit: 10);
-    });
+    feed =
+        fetchFeed("https://unpaprdapi.gargakshit.now.sh/api/feed", limit: 10);
+    lifi = fetchFeed("https://unpaprdapi.gargakshit.now.sh/api/genres/lifi",
+        limit: 10);
+    romance = fetchFeed(
+        "https://unpaprdapi.gargakshit.now.sh/api/genres/romance",
+        limit: 10);
+    plays = fetchFeed("https://unpaprdapi.gargakshit.now.sh/api/genres/plays",
+        limit: 10);
+    scifi = fetchFeed("https://unpaprdapi.gargakshit.now.sh/api/genres/scifi",
+        limit: 10);
+    poetry = fetchFeed("https://unpaprdapi.gargakshit.now.sh/api/genres/poetry",
+        limit: 10);
+    satire = fetchFeed("https://unpaprdapi.gargakshit.now.sh/api/genres/satire",
+        limit: 10);
   }
 
   @override
@@ -36,57 +62,278 @@ class _ExplorePageState extends State<ExplorePage> {
         child: Padding(
           padding: EdgeInsets.symmetric(horizontal: 16.0),
           child: Center(
-            child: RefreshIndicator(
-              onRefresh: refresh,
-              child: ListView(
-                children: <Widget>[
-                  SizedBox(height: 16.0),
-                  Text(
-                    "Explore",
-                    style: GoogleFonts.playfairDisplay(
-                      textStyle: TextStyle(
-                        color: Colors.white,
-                        fontWeight: FontWeight.w900,
-                        fontSize: 32.0,
-                      ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                SizedBox(height: 16.0),
+                Text(
+                  "Explore",
+                  style: GoogleFonts.playfairDisplay(
+                    textStyle: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.w900,
+                      fontSize: 32.0,
                     ),
                   ),
-                  SizedBox(
-                    height: 16.0,
+                ),
+                TabBar(
+                  controller: controller,
+                  isScrollable: true,
+                  tabs: [
+                    Tab(
+                      text: "Random",
+                    ),
+                    Tab(
+                      text: "Literary Fiction",
+                    ),
+                    Tab(
+                      text: "Romance",
+                    ),
+                    Tab(
+                      text: "Plays",
+                    ),
+                    Tab(
+                      text: "Science Fiction",
+                    ),
+                    Tab(
+                      text: "Poetry",
+                    ),
+                    Tab(
+                      text: "Satire",
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 16.0,
+                ),
+                Expanded(
+                  child: TabBarView(
+                    controller: controller,
+                    children: <Widget>[
+                      FutureBuilder<AudiobookShortList>(
+                        future: feed,
+                        builder: (context, snapshot) {
+                          if (snapshot.hasData) {
+                            return ListView(
+                              children: List.generate(
+                                snapshot.data.books.length,
+                                (i) {
+                                  return BookItem(
+                                    audioData: snapshot.data.books[i],
+                                    navigate: widget.navigate,
+                                  );
+                                },
+                              ),
+                            );
+                          } else if (snapshot.hasError) {
+                            return Center(
+                              child: Text(
+                                "Error fetching data, please refresh",
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 16.0,
+                                ),
+                              ),
+                            );
+                          }
+                          return Center(
+                            child: CircularProgressIndicator(),
+                          );
+                        },
+                      ),
+                      FutureBuilder<AudiobookShortList>(
+                        future: lifi,
+                        builder: (context, snapshot) {
+                          if (snapshot.hasData) {
+                            return ListView(
+                              children: List.generate(
+                                snapshot.data.books.length,
+                                (i) {
+                                  return BookItem(
+                                    audioData: snapshot.data.books[i],
+                                    navigate: widget.navigate,
+                                  );
+                                },
+                              ),
+                            );
+                          } else if (snapshot.hasError) {
+                            return Center(
+                              child: Text(
+                                "Error fetching data, please refresh",
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 16.0,
+                                ),
+                              ),
+                            );
+                          }
+                          return Center(
+                            child: CircularProgressIndicator(),
+                          );
+                        },
+                      ),
+                      FutureBuilder<AudiobookShortList>(
+                        future: romance,
+                        builder: (context, snapshot) {
+                          if (snapshot.hasData) {
+                            return ListView(
+                              children: List.generate(
+                                snapshot.data.books.length,
+                                (i) {
+                                  return BookItem(
+                                    audioData: snapshot.data.books[i],
+                                    navigate: widget.navigate,
+                                  );
+                                },
+                              ),
+                            );
+                          } else if (snapshot.hasError) {
+                            return Center(
+                              child: Text(
+                                "Error fetching data, please refresh",
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 16.0,
+                                ),
+                              ),
+                            );
+                          }
+                          return Center(
+                            child: CircularProgressIndicator(),
+                          );
+                        },
+                      ),
+                      FutureBuilder<AudiobookShortList>(
+                        future: plays,
+                        builder: (context, snapshot) {
+                          if (snapshot.hasData) {
+                            return ListView(
+                              children: List.generate(
+                                snapshot.data.books.length,
+                                (i) {
+                                  return BookItem(
+                                    audioData: snapshot.data.books[i],
+                                    navigate: widget.navigate,
+                                  );
+                                },
+                              ),
+                            );
+                          } else if (snapshot.hasError) {
+                            return Center(
+                              child: Text(
+                                "Error fetching data, please refresh",
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 16.0,
+                                ),
+                              ),
+                            );
+                          }
+                          return Center(
+                            child: CircularProgressIndicator(),
+                          );
+                        },
+                      ),
+                      FutureBuilder<AudiobookShortList>(
+                        future: scifi,
+                        builder: (context, snapshot) {
+                          if (snapshot.hasData) {
+                            return ListView(
+                              children: List.generate(
+                                snapshot.data.books.length,
+                                (i) {
+                                  return BookItem(
+                                    audioData: snapshot.data.books[i],
+                                    navigate: widget.navigate,
+                                  );
+                                },
+                              ),
+                            );
+                          } else if (snapshot.hasError) {
+                            return Center(
+                              child: Text(
+                                "Error fetching data, please refresh",
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 16.0,
+                                ),
+                              ),
+                            );
+                          }
+                          return Center(
+                            child: CircularProgressIndicator(),
+                          );
+                        },
+                      ),
+                      FutureBuilder<AudiobookShortList>(
+                        future: poetry,
+                        builder: (context, snapshot) {
+                          if (snapshot.hasData) {
+                            return ListView(
+                              children: List.generate(
+                                snapshot.data.books.length,
+                                (i) {
+                                  return BookItem(
+                                    audioData: snapshot.data.books[i],
+                                    navigate: widget.navigate,
+                                  );
+                                },
+                              ),
+                            );
+                          } else if (snapshot.hasError) {
+                            return Center(
+                              child: Text(
+                                "Error fetching data, please refresh",
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 16.0,
+                                ),
+                              ),
+                            );
+                          }
+                          return Center(
+                            child: CircularProgressIndicator(),
+                          );
+                        },
+                      ),
+                      FutureBuilder<AudiobookShortList>(
+                        future: satire,
+                        builder: (context, snapshot) {
+                          if (snapshot.hasData) {
+                            return ListView(
+                              children: List.generate(
+                                snapshot.data.books.length,
+                                (i) {
+                                  return BookItem(
+                                    audioData: snapshot.data.books[i],
+                                    navigate: widget.navigate,
+                                  );
+                                },
+                              ),
+                            );
+                          } else if (snapshot.hasError) {
+                            return Center(
+                              child: Text(
+                                "Error fetching data, please refresh",
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 16.0,
+                                ),
+                              ),
+                            );
+                          }
+                          return Center(
+                            child: CircularProgressIndicator(),
+                          );
+                        },
+                      ),
+                    ],
                   ),
-                  FutureBuilder<AudiobookShortList>(
-                    future: feed,
-                    builder: (context, snapshot) {
-                      if (snapshot.hasData) {
-                        return Column(
-                          children: List.generate(
-                            snapshot.data.books.length,
-                            (i) {
-                              return BookItem(
-                                audioData: snapshot.data.books[i],
-                                navigate: widget.navigate,
-                              );
-                            },
-                          ),
-                        );
-                      } else if (snapshot.hasError) {
-                        return Center(
-                          child: Text(
-                            "Error fetching data, please refresh",
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 16.0,
-                            ),
-                          ),
-                        );
-                      }
-                      return Center(
-                        child: CircularProgressIndicator(),
-                      );
-                    },
-                  ),
-                ],
-              ),
+                ),
+                SizedBox(
+                  height: 8.0,
+                ),
+              ],
             ),
           ),
         ),
